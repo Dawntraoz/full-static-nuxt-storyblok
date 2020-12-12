cc070592197f87ff6e7ed0126df9969ce55c5426
Dawntraoz <albasilventefu@gmail.com>
2020-12-12 11:54:47 +0100

    Preview mode plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/components'],
+  plugins: ['~/plugins/components', '~/plugins/preview.client.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
