7fac6d9ddad060eade4a5dcaee263995f3ea37e1
Dawntraoz <albasilventefu@gmail.com>
2021-02-17 22:03:53 +0100

    Update nuxt packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,12 @@ export default {
   plugins: ['~/plugins/components', '~/plugins/preview.client.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+  components: [
+    '~/components',
+    '~/components/atoms',
+    '~/components/molecules',
+    '~/components/organisms',
+  ],
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
